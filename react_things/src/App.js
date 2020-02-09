5cf8541c61048ccd4cb747442066276afecba06f
nastinsk <nastinsk@gmail.com>
2020-02-09 03:03:47 -0800

    lab done, readme updated

--- react_things/src/App.js
+++ react_things/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Footer from './Footer';
-import Header from './Header';
-import ThingList from './ThingList';
-import ThingItem from './ThingItem';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import ThingList from './components/ThingList';
+
 import './App.css';
 
 // function App() {
@@ -20,22 +20,31 @@ class App extends React.Component {
                    {name:'b'},
                    {name:'c'}]
     }
+    this.createThingHandler = this.createThingHandler.bind(this);
+    this.deleteThingHandler = this.deleteThingHandler.bind(this);
   }
 
-  submitHandler(event) {
-    alert('A thing was added:  ' );
-    console.log(event.target.value)
-    event.preventDefault();
-  }
   
+ createThingHandler(thing){
+    this.setState({
+     thingsList: this.state.thingsList.concat(thing)
+   })
+ }
+
+ deleteThingHandler(thingToDelete){
  
+    let newThingsLst = this.state.thingsList.filter(thing => thing.name !== thingToDelete.name)
+    
+    this.setState({
+      thingsList: newThingsLst
+    })
+  }
 
   render() {
     return (
       <div className="App">
       <Header greeting = "welcome" count = {this.state.thingsList.length}/>
-      <ThingList thingsList={this.state.thingsList}/>
-      <ThingItem onSubmit={this.submitHandler} />
+      <ThingList thingsList={this.state.thingsList} onCreated={this.createThingHandler} onDelete={this.deleteThingHandler}/>
       <Footer />
       </div>
     )
--- react_things/src/components/Header.js
+++ react_things/src/components/Header.js
@@ -3,17 +3,14 @@ import React from 'react';
 class Header extends React.Component {
   constructor(props){
     super(props);
-    this.state = {
-      greeting: props.greeting,
-      count: props.count
-    }
+   
   }
 
   render() {
     return (
       <>
-      <h1>{this.state.greeting}</h1>
-      <p>You have {this.state.count} things</p>
+      <h1>{this.props.greeting}</h1>
+      <p>You have {this.props.count} poducts</p>
       </>
     )
   }
